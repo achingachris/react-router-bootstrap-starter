a40024edc5df02c11dcaf6aa6e97ef5aa0e3a02a
ChrisAchinga <achinga.chris@gmail.com>
2020-12-25 16:51:52 +0300

    use react-helmet for title tags

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,13 @@
+import {Helmet} from "react-helmet";
 import AboutHeader from '../components/AboutHeader'
 
 const AboutPage = () => {
   return (
     <>
+    <Helmet>
+        <title>About Page</title>
+        <meta name="description" content="Helmet application" />
+    </Helmet>
       <AboutHeader />
     </>
   )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,13 @@
+import {Helmet} from "react-helmet";
 import Header from '../components/Header'
 
 const HomePage = () => {
   return (
     <div>
+      <Helmet>
+        <title>Home Page</title>
+        <meta name="description" content="Helmet application" />
+    </Helmet>
       <Header />
     </div>
   )
