c4606578c26925640ccc902849b591f5e19f729f
ChrisAchinga <achinga.chris@gmail.com>
2021-03-01 17:31:57 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+// page components
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+
 const App = () => {
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <Router>
+      <Menu />
+      <main>
+        <Container>
+          <Route path='/' component={Home} exact />
+          <Route path='/about' component={About} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
