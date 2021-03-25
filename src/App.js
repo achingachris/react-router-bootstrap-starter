6bc7e174c1dae81156dd9d7612b155b9991d4bbb
ChrisAchinga <achinga.chris@gmail.com>
2021-03-25 17:21:06 +0300

    404 error page handling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // page components
 import Menu from './components/Menu'
 import Footer from './components/Footer'
-
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
@@ -15,8 +14,11 @@ const App = () => {
       <Menu />
       <main>
         <Container>
-          <Route path='/' component={Home} exact />
-          <Route path='/about' component={About} />
+          <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/about' component={About} />
+            <Route component={NotFound} />
+          </Switch>
         </Container>
       </main>
       <Footer />
---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            Sorry, Page not Found
+        </div>
+    )
+}
+
+export default NotFound
