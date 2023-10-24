bb923bd50e80e61992717d015bb21d8d0f97f98a
doghot <achinga.chris@gmail.com>
2023-10-24 10:31:40 +0300

    Minor Updates on Syntax, updating node packages and minor bug fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
-import { Container } from 'react-bootstrap'
+import { Container } from "react-bootstrap";
 
 const Header = ({ head, description }) => {
   return (
     <Container>
-      <div className='starter-template text-center mt-5'>
+      <div className="starter-template text-center mt-5">
         <h1>{head} Page</h1>
-        <p className='lead text-capitalize'>{description}</p>
+        <p className="lead text-capitalize">{description}</p>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Header
+Header.defaultProps = {
+  head: "Default Head",
+  description: "Default description",
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import Header from '../components/Header'
-import Meta from '../components/Meta'
+import Header from "../components/Header";
+import Meta from "../components/Meta";
 
 const Home = () => {
   // page content
-  const pageTitle = 'Home'
-  const pageDescription = 'welcome to react bootstrap template'
+  const pageTitle = "Home";
+  const pageDescription = "welcome to react bootstrap template";
 
   return (
     <div>
-      <Meta title={pageTitle}/>
+      <Meta title={pageTitle} />
       <Header head={pageTitle} description={pageDescription} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
